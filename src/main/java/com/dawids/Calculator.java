cffafcc471a5cff27342d72440615b01d78deae2
dawids21 <dawid.stasiak21@gmail.com>
2020-05-20 08:55:11 +0200

    Add number buttons action

--- src/main/java/com/dawids/Calculator.java
+++ src/main/java/com/dawids/Calculator.java
@@ -45,7 +45,9 @@ public class Calculator extends GridPane {
         fieldActual.prefWidthProperty().bind(widthProperty().divide(5).multiply(4));
         fieldActual.setFont(font);
         fieldActual.styleProperty().bind(Bindings.concat("-fx-font-size: ", fontSize.asString()));
+        fieldActual.setAlignment(Pos.CENTER_RIGHT);
         fieldActual.setEditable(false);
+        //TODO add converter
 
         add(fieldMemory, 0, 0, 2, 1);
         add(fieldResult, 2, 0, 3, 1);
@@ -63,53 +65,93 @@ public class Calculator extends GridPane {
 
     private void createButtons() {
         buttons[0] = new Button("MC");
+
         buttons[1] = new Button("MR");
+
         buttons[2] = new Button("M+");
+
         buttons[3] = new Button("M-");
+
         buttons[4] = new Button("%");
         buttons[4].setOnAction(event -> fieldOperation.setText("%"));
+
         buttons[5] = new Button("7");
+        buttons[5].setOnAction(event -> addToActual("7"));
+
         buttons[6] = new Button("8");
+        buttons[6].setOnAction(event -> addToActual("8"));
+
         buttons[7] = new Button("9");
+        buttons[7].setOnAction(event -> addToActual("9"));
+
         buttons[8] = new Button("+");
         buttons[8].setOnAction(event -> fieldOperation.setText("+"));
+
         buttons[9] = new Button("-");
         buttons[9].setOnAction(event -> fieldOperation.setText("-"));
+
         buttons[10] = new Button("4");
+        buttons[10].setOnAction(event -> addToActual("4"));
+
         buttons[11] = new Button("5");
+        buttons[11].setOnAction(event -> addToActual("5"));
+
         buttons[12] = new Button("6");
+        buttons[12].setOnAction(event -> addToActual("6"));
+
         buttons[13] = new Button("*");
         buttons[13].setOnAction(event -> fieldOperation.setText("*"));
+
         buttons[14] = new Button("/");
         buttons[14].setOnAction(event -> fieldOperation.setText("/"));
+
         buttons[15] = new Button("1");
+        buttons[15].setOnAction(event -> addToActual("1"));
+
         buttons[16] = new Button("2");
+        buttons[16].setOnAction(event -> addToActual("2"));
+
         buttons[17] = new Button("3");
+        buttons[17].setOnAction(event -> addToActual("3"));
+
         buttons[18] = new Button("^");
         buttons[18].setOnAction(event -> fieldOperation.setText("^"));
+
         buttons[19] = new Button("√");
         buttons[19].setOnAction(event -> fieldOperation.setText("√"));
+
         buttons[20] = new Button("+/-");
-        buttons[20].setOnAction(event -> fieldOperation.setText("+/-"));
+
         buttons[21] = new Button("0");
+        buttons[21].setOnAction(event -> addToActual("0"));
+
         buttons[22] = new Button(".");
+
         buttons[23] = new Button("C");
         buttons[23].setOnAction(event -> {
             fieldActual.clear();
             fieldOperation.clear();
         });
+
         buttons[24] = new Button("=");
+
         buttons[25] = new Button("AC");
+
         buttons[25].setOnAction(event -> {
             fieldActual.clear();
             fieldOperation.clear();
             fieldResult.clear();
         });
+
         for (Button button : buttons) {
             button.prefHeightProperty().bind(heightProperty().divide(8));
             button.prefWidthProperty().bind(widthProperty().divide(5));
             button.styleProperty().bind(Bindings.concat("-fx-font-size: ", fontSize.asString()));
             button.setFont(font);
         }
+    }
+
+    private void addToActual(String text) {
+        fieldActual.setText(fieldActual.getText() + text);
     }
 }
